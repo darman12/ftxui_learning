b927f9ae9124a92e48bc3272cceb4162439b7a43
asthmatichorse <213134211+asthmatichorse@users.noreply.github.com>
2025-06-09 10:33:07 -0500

    removed second executable from cmake and updated .gitignore to remove build artifacts from repo

--- main.cpp
+++ main.cpp
@@ -3,15 +3,17 @@
 #include <iostream>
 
 int main() {
-    using namespace ftxui;
+  using namespace ftxui;
 
-    Element document = hbox({
-      text("left")   | border,
+  Element document = hbox({
+      text("left") | border,
       text("middle") | border | flex,
-      text("right")  | border,
-    });
+      text("right") | border,
+  });
 
-    auto screen = Screen::Create(Dimension::Full(), Dimension::Fit(document));
-    Render(screen, document);
-    screen.Print();
+  auto screen = Screen::Create(Dimension::Full(), Dimension::Fit(document));
+  Render(screen, document);
+  screen.Print();
+
+  return 0;
 }
